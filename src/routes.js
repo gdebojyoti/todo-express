42678e038f1b5b8fe837987cf6bb5f5552c07460
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2019-10-13 11:42:56 +0530

    Data sync.

--- src/routes.js
+++ src/routes.js
@@ -41,6 +41,76 @@ const appRouter = function (app) {
     })
   })
 
+  app.use('/syncTodos', async (req, res) => {
+    const { todos, email, token } = req.body || {}
+    const cArr = todos || []
+
+    // fetch array of todos from server (sArr)
+    const user = await DatabaseService.getAuthorizedUser(email, token)
+    if (!user) {
+      res.status(403).send({ msg: 'unauthorized user' })
+    }
+    const data = await DatabaseService.fetchTodos(user.id)
+    const sArr = data ? data.todos : []
+
+    // compare cArr with sArr; generate new list of todos nArr
+    cArr.forEach(todo => {
+      // compare client todo
+      // @TODO: check for invalid todo item
+      if (!todo || typeof todo !== 'object') {
+        return
+      }
+      if (!todo.id) {
+        return
+      }
+
+      const index = sArr.findIndex(item => item.id === todo.id)
+      // if todo is new, push it to server todo array
+      if (index === -1) {
+        sArr.push({ ...todo, isSynced: true })
+        return
+      }
+
+      let itemInServer = sArr[index]
+
+      // ignore if item on server is already deleted
+      if (itemInServer.isDeleted) {
+        return
+      }
+
+      // if todo has just been deleted, delete entire server item except id & isDeleted
+      if (todo.isDeleted) {
+        itemInServer = {
+          id: todo.id,
+          isDeleted: true
+        }
+      }
+
+      // if client todo is more recent, replace server todo with it
+      if (!itemInServer.lastUpdated || todo.lastUpdated > itemInServer.lastUpdated) {
+        itemInServer = { ...todo, isSynced: true }
+      }
+
+      // update server item details in server todos array
+      sArr[index] = itemInServer
+    })
+
+    const updatedData = {
+      ...data,
+      userId: user.id,
+      todos: sArr
+    }
+
+    // save updatedData to DB
+    const saved = await DatabaseService.saveTodos(updatedData)
+    if (!saved) {
+      res.status(400).send({ msg: 'data sync failed' })
+    }
+
+    // send it to client
+    res.send({ todos: sArr })
+  })
+
   app.use('/', (req, res) => {
     res.send('Nothing to see here...')
   })
--- src/services/db.js
+++ src/services/db.js
@@ -21,6 +21,7 @@ class DatabaseService {
 
       const database = client.db('thetodoapp')
       this.users = database.collection('users')
+      this.todos = database.collection('todos')
 
       // const isExistingUser = await this.isExistingUser('djhf')
       // console.log(isExistingUser)
@@ -46,9 +47,35 @@ class DatabaseService {
     return user
   }
 
+  // fetch authorized user (one that matches both email & token)
+  async getAuthorizedUser (email, token) {
+    if (!email || !token) {
+      return null
+    }
+
+    const user = await this.users.findOne({ email, token })
+    return user
+  }
+
   async addUser (details) {
     const added = await this.users.insertOne(details)
     return added
+  }
+
+  // fetch list of all todos for given user
+  async fetchTodos (userId) {
+    const data = await this.todos.findOne({ userId })
+    return data
+  }
+
+  async saveTodos (data) {
+    try {
+      await this.todos.updateOne({ userId: data.userId }, { $set: data }, { upsert: true })
+      return true
+    } catch (e) {
+      console.log('Could not update DB', e)
+      return false
+    }
   }
 }
 
