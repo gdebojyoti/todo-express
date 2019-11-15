dd582da12d34b315c698f93623f195a5a1bd53c6
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2019-11-15 23:12:11 +0530

    Domain list updated.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   PORT: process.env.PORT || 80,
-  ALLOWED_ORIGINS: ['http://localhost:9876', 'http://xenspaces.xyz', 'https://thetodo.app'],
+  ALLOWED_ORIGINS: ['http://localhost:9876', 'https://thetodo.app'],
   GOOGLE_CLIENT_ID: '595981444577-j9jv1jjbgn0iba1th87lcmmqnll83stc.apps.googleusercontent.com',
   MONGO_URL: 'mongodb://tropicano:steiffme1sterr@ds046037.mlab.com:46037/thetodoapp'
 }
